d11c3e9de5cb290385b6cbcba827c3c25a29a60d
Henrique Brighenti <hbrighen@thoughtworks.com>
2017-07-04 18:06:34 -0300

    Change the testing strategy and folder organization/structure
    Now using jest that was already installed, and saving the specs file in the same folder as the component it is testing.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RaisedButton from 'material-ui/RaisedButton';
 
@@ -8,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="./logo.svg" className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 

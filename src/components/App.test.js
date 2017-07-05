00f622a57ae41706fb7f47b56d64313020608f44
Adriano Godoy <agodoy@thoughtworks.com>
2017-07-05 17:18:05 -0300

    Fix lint errors

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import RaisedButton from 'material-ui/RaisedButton';
 
 it('renders without crashing', () => {

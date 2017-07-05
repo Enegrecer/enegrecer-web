cde735208830cfcb5ea4a7ad34897e11b1e223ae
Henrique Brighenti <hbrighen@thoughtworks.com>
2017-07-05 17:23:33 -0300

    Separate styles to its own file for the login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './Login.style';
 
 export default class Login extends React.Component {
     render() {
         return (
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "20" }}>
-                <div style={{ marginTop: "20" }}>
+            <div style={styles.wrapper}>
+                <div style={styles.input}>
                     <p>Login</p>
                     <input id="login"></input>
                 </div>
-                <div style={{ marginTop: "20" }}>
+                <div style={styles.input}>
                     <p>Password</p>
                     <input id="password" type="password"></input>
                 </div>
---
+++ src/components/login/Login.style.js
@@ -0,0 +1,12 @@
+export default {
+    wrapper: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "20"
+    },
+
+    input: {
+        marginTop: "20"
+    }
+}

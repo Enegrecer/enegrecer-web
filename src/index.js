a441809b2c573377fb106be06a15788cc8a42ec1
Adriano Godoy <agodoy@thoughtworks.com>
2017-07-05 15:35:49 -0300

    Initial lint config

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 ReactDOM.render(
-    <MuiThemeProvider>
+ <MuiThemeProvider>
         <App />
     </MuiThemeProvider>,
   document.getElementById('root')

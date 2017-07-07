3aa9479a4947c532fabdbc70a94c2ec3aa9c1c1a
Paulo Gama <pgama@thoughtworks.com>
2017-07-07 18:37:34 -0300

    Adicionando Card do MaterialUI

--- src/components/categories/card/CategoriesCard.test.js
+++ src/components/categories/card/CategoriesCard.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CategoriesCard from './CategoriesCard';
 
-it('renders without crashing', () => {
-	const wrapper = shallow(<CategoriesCard />);
-	expect(wrapper.find('div').exists()).toBe(true);
+describe('<CategoriesCard />', () => {
+	it('have a card component', () => {
+		const wrapper = shallow(<CategoriesCard />);
+		expect(wrapper.find('Card').exists()).toBe(true);
+	});
+
 });
--- src/components/categories/list/CategoriesList.test.js
+++ src/components/categories/list/CategoriesList.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CategoriesList from './CategoriesList';
-import CategoriesCard from '../card/CategoriesCard';
 
 describe('<CategoriesList />', () => {
 
 	it('has CategoriesCard', () => {
 		const wrapper = shallow(<CategoriesList />);
-		expect(wrapper.containsMatchingElement(<CategoriesCard />)).toBe(true);
+		expect(wrapper.find('CategoriesCard').exists()).toBe(true);
 	});
 
 });

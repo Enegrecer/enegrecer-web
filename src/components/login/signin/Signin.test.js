abeab69d5cd88f0aee829bf6794b00176185f754
Henrique Brighenti <hbrighen@thoughtworks.com>
2017-07-10 11:41:34 -0300

    Adiciona testes para o componente de signin

--- src/components/login/signin/Signin.test.js
+++ src/components/login/signin/Signin.test.js
@@ -2,7 +2,49 @@ import React from 'react';
 import Signin from './Signin';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-    const wrapper = shallow(<Signin />);
-    expect(wrapper.exists()).toBe(true);
+describe('Signin Component', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<Signin />);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('initialize the state object properly', () => {
+        const wrapper = shallow(<Signin />);
+        const expetctedState = {
+            logged: false,
+            login: '',
+            password: ''
+        };
+        expect(wrapper.state()).toEqual(expetctedState);
+    });
+
+    describe('setProperty method', () => {
+        it('set a property in the state', () => {
+            const wrapper = shallow(<Signin />);
+            const simulatedEvent = {
+                target: {
+                    value: true
+                }
+            };
+            const propertyToSet = 'logged';
+            wrapper.instance().setProperty(simulatedEvent, propertyToSet);
+            expect(wrapper.state(propertyToSet)).toBe(simulatedEvent.target.value);
+        });
+
+        it('does not override other properties of the state', () => {
+            const wrapper = shallow(<Signin />);
+            const propertyToSet = 'logged';
+            const simulatedEvent = {
+                target: {
+                    value: true
+                }
+            };
+
+            let expetctedState = wrapper.state();
+            expetctedState[propertyToSet] = simulatedEvent.target.value;
+
+            wrapper.instance().setProperty(simulatedEvent, propertyToSet);
+            expect(wrapper.state()).toEqual(expetctedState);
+        });
+    });
 });

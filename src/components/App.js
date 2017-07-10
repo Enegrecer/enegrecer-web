3af610ee620db5db1c1857ae2e2fe03f15100314
Paulo Gama <gamaufla@gmail.com>
2017-07-09 22:29:17 -0300

    [Incomplete] Trying to fetch data from firebase and store on array

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Login from './login/Login';
+import CategoriesList from './categories/list/CategoriesList';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Login />
+                <CategoriesList />
             </div>
         );
     }
--- src/components/categories/list/CategoriesList.js
+++ src/components/categories/list/CategoriesList.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
-import CategoriesCard from '../card/CategoriesCard';
+// import CategoriesCard from '../card/CategoriesCard';
+import firebaseApp from '../../../utils/firebaseUtils';
+import {GridList, GridTile} from 'material-ui/GridList';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import IconButton from 'material-ui/IconButton';
 
 export default class CategoriesList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: []
+    };
+
+    var categories = [];
+    this.firebaseRef = firebaseApp.database().ref('categories');
+    this.firebaseRef.on('value', function(snapshot) {
+      snapshot.forEach((child) => {
+        categories.push({
+          key: child.key,
+          description: child.val().descricao
+        });
+      });
+
+      console.log(categories);
+    }.bind(this));
+
+  }
+
   render() {
     return (
-			<CategoriesCard />
+      // <div style={styles.root}>
+      // <GridList
+      //   cellHeight={180}
+      //   style={styles.gridList}
+      // >
+      //   <Subheader>December</Subheader>
+      //     {this.state.categories.map((tile) => (
+      //     <GridTile
+      //       key={tile.title}
+      //       title={tile.title}
+      //       subtitle={<span>by <b>{tile.author}</b></span>}
+      //       actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+      //     >
+      //     </GridTile>
+      //   ))}
+      // </GridList>
+      // </div>
+      <h2>{this.state.categories}</h2>
     );
   }
 }
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+};

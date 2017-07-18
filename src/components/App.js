4b7964802203dd778ce4d528ddfc15e343be39dd
Adriano Godoy <agodoy@thoughtworks.com>
2017-07-18 15:52:40 -0300

    Instala react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import CategoriesList from './categories/list/CategoriesList';
+import Login from './login/Login';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import CategoriesList from './categories/list/CategoriesList';
+
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <CategoriesList />
-            </div>
+            <BrowserRouter>         
+                <Switch>       
+                    <Route exact path="/" component={Login} />
+                    <Route path="/categorias" component={CategoriesList} />
+                </Switch>            
+            </BrowserRouter>
         );
     }
 }

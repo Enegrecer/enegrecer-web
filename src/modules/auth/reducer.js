a667952846e2036040f7c9a46aa45aac32b9c42a
Henrique Brighenti <hbrighen@thoughtworks.com>
2017-08-23 18:20:35 -0300

    Salva no estado o id do usuário logado, na ação de sucesso de login

--- src/modules/auth/reducer.js
+++ src/modules/auth/reducer.js
@@ -3,7 +3,11 @@ import { SUCCESS_SIGN_IN, REQUEST_SIGN_IN, FAILURE_SIGN_IN } from '../../actions
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case SUCCESS_SIGN_IN:
-      return { ...state, logged: true };
+      return { 
+        ...state, 
+        logged: true,
+        userId: action.payload.user
+       };
 
     case REQUEST_SIGN_IN:
     case FAILURE_SIGN_IN:

2ac23bda1630ecef726c2bd7658863f0a7b2964a
Guilherme Corsino <guilhermecorsino@outlook.com>
2017-09-04 16:31:13 -0300

    Junta a versão de rotas na branch

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,13 @@
 import { createAction } from 'redux-actions';
 
+
+export const REQUEST_SIGN_IN = 'REQUEST_SIGN_IN';
+export const SUCCESS_SIGN_IN = 'SUCCESS_SIGN_IN';
+export const FAILURE_SIGN_IN = 'FAILURE_SIGN_IN';
+export const requestSignIn = createAction(REQUEST_SIGN_IN);
+export const successSignIn = createAction(SUCCESS_SIGN_IN);
+export const failureSignIn = createAction(FAILURE_SIGN_IN);
+
 export const REQUEST_CREATE_COMPLAINT = 'REQUEST_CREATE_COMPLAINT';
 export const SUCCESS_CREATE_COMPLAINT = 'SUCCESS_CREATE_COMPLAINT';
 export const FAILURE_CREATE_COMPLAINT = 'FAILURE_CREATE_COMPLAINT';

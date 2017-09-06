9917f8266894b7753b613330e935e31292dde808
Guilherme Corsino <guilhermecorsino@outlook.com>
2017-09-06 12:01:14 -0300

    Adiciona um componente de formulário para a criação da denúncia

---
+++ src/components/complaints/NewComplaintForm.js
@@ -0,0 +1,94 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+
+export default class NewComplaintForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      report: '',
+      ocurrenceDate: '',
+      categoryId: '',
+      address: '',
+      latitude: '',
+      longitude: '',
+    };
+  }
+
+  onPressSaveButton() {
+    this.props.salvarDenuncia(this.state);
+  }
+
+  setProperty(event, property) {
+    const currentState = this.state;
+    this.setState({
+      ...currentState,
+      [property]: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Nova Denúncia</h1>
+        <label htmlFor="report">Detalhamento:</label>
+
+        <TextField
+          id="report"
+          value={this.state.report}
+          onChange={e => this.setProperty(e, 'report')}
+        />
+
+        <label htmlFor="ocurrenceDate">Data e Hora do ocorrido:</label>
+        <TextField
+          id="occurenceDate"
+          value={this.state.ocurrenceDate}
+          onChange={e => this.setProperty(e, 'ocurrenceDate')}
+        />
+
+        <label htmlFor="categoryId">Categoria (injury ou racism)</label>
+        <TextField
+          id="categoryId"
+          value={this.state.categoryId}
+          onChange={e => this.setProperty(e, 'categoryId')}
+        />
+
+        <h3>Local do crime</h3>
+        <br />
+
+        <label htmlFor="address">Endereço:</label>
+        <TextField
+          id="address"
+          value={this.state.address}
+          onChange={e => this.setProperty(e, 'address')}
+        />
+
+        <br />
+
+        <label htmlFor="latitude">Latitude:</label>
+        <TextField
+          id="latitude"
+          value={this.state.latitude}
+          onChange={e => this.setProperty(e, 'latitude')}
+        />
+        <br />
+
+
+        <label htmlFor="longitude">Longitude:</label>
+        <TextField
+          id="longitude"
+          value={this.state.longitude}
+          onChange={e => this.setProperty(e, 'longitude')}
+        />
+        <br />
+
+        <button onClick={() => this.onPressSaveButton()}>
+              Salvar
+        </button>
+      </div>);
+  }
+}
+
+NewComplaintForm.propTypes = {
+  salvarDenuncia: PropTypes.func.isRequired,
+};
--- src/containers/complaints/NewComplaintContainer.js
+++ src/containers/complaints/NewComplaintContainer.js
@@ -2,104 +2,32 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import TextField from 'material-ui/TextField';
 import { push } from 'react-router-redux';
 import { requestCreateComplaint } from '../../actions';
+import NewComplaintForm from '../../components/complaints/NewComplaintForm';
 
 export class NewComplaintContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      report: '',
-      ocurrenceDate: '',
-      categoryId: '',
-      address: '',
-      latitude: '',
-      longitude: '',
-    };
+    this.onPressSaveButton = this.onPressSaveButton.bind(this);
   }
 
-  onPressSaveButton() {
+  onPressSaveButton(state) {
     this.props.requestCreateComplaint({
-      ...this.state,
+      ...state,
       informer: this.props.informer,
       onSuccess: push('/painel'),
     });
   }
 
-  setProperty(event, property) {
-    const currentState = this.state;
-    this.setState({
-      ...currentState,
-      [property]: event.target.value,
-    });
-  }
-
   render() {
-    return (
-      <div>
-        <h1>Nova Denúncia</h1>
-        <label htmlFor="report">Detalhamento:</label>
-
-        <TextField
-          id="report"
-          value={this.state.report}
-          onChange={e => this.setProperty(e, 'report')}
-        />
-
-        <label htmlFor="ocurrenceDate">Data e Hora do ocorrido:</label>
-        <TextField
-          id="occurenceDate"
-          value={this.state.ocurrenceDate}
-          onChange={e => this.setProperty(e, 'ocurrenceDate')}
-        />
-
-        <label htmlFor="categoryId">Categoria (injury ou racism)</label>
-        <TextField
-          id="categoryId"
-          value={this.state.categoryId}
-          onChange={e => this.setProperty(e, 'categoryId')}
-        />
-
-        <h3>Local do crime</h3>
-        <br />
-
-        <label htmlFor="address">Endereço:</label>
-        <TextField
-          id="address"
-          value={this.state.address}
-          onChange={e => this.setProperty(e, 'address')}
-        />
-
-        <br />
-
-        <label htmlFor="latitude">Latitude:</label>
-        <TextField
-          id="latitude"
-          value={this.state.latitude}
-          onChange={e => this.setProperty(e, 'latitude')}
-        />
-        <br />
-
-
-        <label htmlFor="longitude">Longitude:</label>
-        <TextField
-          id="longitude"
-          value={this.state.longitude}
-          onChange={e => this.setProperty(e, 'longitude')}
-        />
-        <br />
-
-        <button onClick={() => this.onPressSaveButton()}>
-              Salvar
-        </button>
-      </div>);
+    return <NewComplaintForm salvarDenuncia={this.onPressSaveButton} />;
   }
 }
 
 NewComplaintContainer.propTypes = {
-  requestCreateComplaint: PropTypes.func.isRequired,
   informer: PropTypes.string.isRequired,
+  requestCreateComplaint: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({

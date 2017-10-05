f331fe4388c90174da90f2f129a6d563cbbfa0e0
Gabriel Pereira <gspereir@thoughtworks.com>
2017-10-05 14:04:23 -0300

    fix - ES lint

--- src/components/Rotas.js
+++ src/components/Rotas.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import HomePage from './home/Home';
 import Login from './login/Login';
@@ -37,5 +38,9 @@ const Rotas = props => (
     </Painel>
   </Switch>
 );
+
+Rotas.propTypes = {
+  authed: PropTypes.bool.isRequired,
+};
 
 export default Rotas;

8dfcdea62e8f163f010a06146c9c1e4c0687f988
Gabriel Pereira <gspereir@thoughtworks.com>
2017-10-07 18:51:12 -0300

    Remove console.log

--- src/components/layouts/Painel.js
+++ src/components/layouts/Painel.js
@@ -42,7 +42,6 @@ class Painel extends React.Component {
           <Link to="/"><MenuItem>Home</MenuItem></Link>
           <Link to="/painel/categorias"><MenuItem>Categorias</MenuItem></Link>
           <Link to="/painel/denuncias/nova"><MenuItem>Criar Denúncia</MenuItem></Link>
-          {console.log(this.props.children[0].props.authed)}
           {
             this.props.children[0].props.authed
               ? <Link to="#" onClick={logout}><MenuItem>Logout</MenuItem></Link>

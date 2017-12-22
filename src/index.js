fbc683c6521b20ad2b997ad1afab1305361e55df
Daniel Faria <lenin@stack.com>
2017-12-22 17:46:20 -0200

    Issue #82 Refatoracao do Arquivo src/index.js, arquivo de configuracao dividido em outras partes, pertinente a cada biblioteca da aplicação

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import { createStore, applyMiddleware, compose } from 'redux';
-
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-
-import registerServiceWorker from './utils/registerServiceWorker';
-
-import reducers from './reducers';
-import rootSaga from './sagas';
 import './assets/styles/index.css';
 
+import registerServiceWorker from './utils/registerServiceWorker';
+import configureStore from './store'
 import App from './components/App'
 
 
-const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = composeEnhancers(applyMiddleware(sagaMiddleware));
-const store = createStore(reducers, middlewares);
-sagaMiddleware.run(rootSaga);
+const store = configureStore();
 
-// Needed for onTouchTap
 injectTapEventPlugin();
 
 ReactDOM.render(

32cef08e510324846df004a04d9e65d61c03356a
Daniel Faria <lenin@stack.com>
2017-12-21 22:03:05 -0200

    [DANIEL] Criando o arquivo routes/Routes.js, Removida a TAG History e sua dependência do package.json- Ainda falta migrar as rotas duplicadas em Rotas.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { ConnectedRouter, routerMiddleware as createRouterMiddleware } from 'react-router-redux';
-import { createBrowserHistory } from 'history';
+
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
 import registerServiceWorker from './utils/registerServiceWorker';
-import App from './components/App';
+
 import reducers from './reducers';
 import rootSaga from './sagas';
 import './assets/styles/index.css';
 
-const history = createBrowserHistory();
+import Routes from './routes/Routes'
+
+
 const sagaMiddleware = createSagaMiddleware();
-const routerMiddleware = createRouterMiddleware(history);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = composeEnhancers(applyMiddleware(sagaMiddleware, routerMiddleware));
+const middlewares = composeEnhancers(applyMiddleware(sagaMiddleware));
 const store = createStore(reducers, middlewares);
 sagaMiddleware.run(rootSaga);
 
@@ -30,9 +30,7 @@ injectTapEventPlugin();
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <ConnectedRouter history={history}>
-        <App history={history} />
-      </ConnectedRouter>
+      <Routes />
     </MuiThemeProvider>
   </Provider>
   ,

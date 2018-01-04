9978bb87d0a3d035789911795991d519fc3858ea
Daniel Faria <lenin@stack.com>
2018-01-04 15:07:27 -0200

    :art: correção na importação de denunciaReducer no combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { denuncias } from './denunciaReducer';
+import  denuncias  from './denunciaReducer';
 
 export default combineReducers({
   router: routerReducer, denuncias

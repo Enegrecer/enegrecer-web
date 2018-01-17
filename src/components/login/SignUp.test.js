5a03c733a4b426c5d8ea2223fadb843d7a469bc2
Luciana Montenegro <lmontene@thoughtworks.com>
2018-01-17 16:05:05 -0300

    [Kamila/Luciana] Tradução dos testes e adição do teste de redirecionamento após registrar usuário

--- src/components/login/SignUp.test.js
+++ src/components/login/SignUp.test.js
@@ -3,16 +3,18 @@ import { shallow } from 'enzyme';
 import SignUp from './SignUp';
 import * as firebase from '../../utils/firebaseUtils';
 import { setProperty } from '../../modules/auth';
+import { mock } from 'sinon'
+import NovaDenunciaForm from '../denuncias/NovaDenunciaForm';
 
 jest.mock('../../utils/firebaseUtils');
 
 describe('SignUp component', () => {
-  it('renders without crashing', () => {
+  it('Teste de renderização', () => {
     const wrapper = shallow(<SignUp />);
     expect(wrapper.exists()).toBe(true);
   });
 
-  it('initializes the state properly', () => {
+  it('Inicializa o state corretamente', () => {
     const wrapper = shallow(<SignUp />);
     const expectedState = {
       logged: false,
@@ -25,7 +27,7 @@ describe('SignUp component', () => {
   });
 
   describe('setProperty method', () => {
-    it('set a property in the state', () => {
+    it('Set uma propriedade no state', () => {
       const wrapper = shallow(<SignUp />);
       const simulatedEvent = {
         target: {
@@ -37,7 +39,7 @@ describe('SignUp component', () => {
       expect(wrapper.state(propertyToSet)).toBe(simulatedEvent.target.value);
     });
 
-    it('does not override other properties of the state', () => {
+    it('Não sobrescreve outras propriedades no state', () => {
       const wrapper = shallow(<SignUp />);
       const propertyToSet = 'logged';
       const simulatedEvent = {
@@ -59,7 +61,7 @@ describe('SignUp component', () => {
       firebase.userObj.sendEmailVerification.mockClear();
     });
 
-    it('sets the state with "logged" to true when registration is sucessfull', async () => {
+    it('Deveria retornar o status logado "logged = true" do usuário após o cadastro', async () => {
       const wrapper = shallow(<SignUp />);
       wrapper.setState({
         login: 'pass',
@@ -69,7 +71,7 @@ describe('SignUp component', () => {
       expect(wrapper.state('logged')).toBe(true);
     });
 
-    it('sets the state with the error variables when the login fails', async () => {
+    it('Seta as variáreis de erro do state quando o cadastro falhar', async () => {
       const wrapper = shallow(<SignUp />);
       wrapper.setState({
         login: 'error',
@@ -82,7 +84,7 @@ describe('SignUp component', () => {
       expect(wrapper.state('password')).toEqual('');
     });
 
-    it('should call the "sendEmailVerification" method when creating a user', async () => {
+    it('Deveria chamar o método "sendEmailVerification" ao criar um usuário', async () => {
       const wrapper = shallow(<SignUp />);
       wrapper.setState({
         login: 'pass',
@@ -92,4 +94,22 @@ describe('SignUp component', () => {
       expect(firebase.userObj.sendEmailVerification.mock.calls.length).toBe(1);
     });
   });
+
+
+    describe("Testes de funcionalidade para o registro de usuários", () => {
+      it('Deveria redirecionar para a página de denúncia quando um usário for cadastrado com sucesso', () => {
+        const wrapper = shallow(<SignUp />);
+        wrapper.setState({
+          firstName: 'firstName',
+          lastName: 'lastName',
+          login: 'b@b.com',
+          password: 'senhaComMaisDe6Caracteres',
+          logged: false,
+        });
+        wrapper.instance().onPressRegisterButton();
+        const tela = shallow(<NovaDenunciaForm salvarDenuncia={mock()} />);
+        expect(tela.find('NovaTestemunhaForm')).toHaveLength(1)
+      });
+    });
+
 });

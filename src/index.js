6dd870b50987db2da79111640d1e10e773896128
Daniel Faria <lenin@stack.com>
2018-01-17 14:43:09 -0200

    :art remoção do index a configurção do redux/saga e colocadas em arquivos especiais

--- src/index.js
+++ src/index.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { createStore, applyMiddleware, compose } from 'redux';
 
-import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
+import configureStore from './store';
+import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker';
 
-import reducers from './reducers';
-import rootSaga from './sagas';
 import './assets/styles/index.css';
-
-import App from './components/App'
-
-
-const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = composeEnhancers(applyMiddleware(sagaMiddleware));
-const store = createStore(reducers, middlewares);
-sagaMiddleware.run(rootSaga);
 
 // Needed for onTouchTap
 injectTapEventPlugin();
+
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>

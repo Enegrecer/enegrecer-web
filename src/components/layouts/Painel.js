7e835f9c8b2662a4ac52b7cbab20d6bdc54c23d7
Daniel Faria <lenin@stack.com>
2018-02-26 01:12:46 -0300

    Conversão do componente painel pasra stateless

--- src/components/layouts/Painel.js
+++ src/components/layouts/Painel.js
@@ -3,28 +3,20 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-class Painel extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <nav>
-          <div className="nav-wrapper">
-            <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <li> <Link to="/">Home</Link></li>
-              <li> <Link to="/painel/denuncias/nova">Criar Denúncia</Link></li>
-            </ul>
-          </div>
-        </nav>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
+const Painel = (props) => (
+    <div>
+      <nav>
+        <div className="nav-wrapper">
+          <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <li> <Link to="/">Home</Link></li>
+            <li> <Link to="/painel/denuncias/nova">Criar Denúncia</Link></li>
+          </ul>
+        </div>
+      </nav>
+      {props.children}
+    </div>
+);
+  
 Painel.propTypes = {
   children: PropTypes.element.isRequired
 };

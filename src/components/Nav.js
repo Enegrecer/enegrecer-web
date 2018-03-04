ad4e766ec878363d251c4d262eb530339b64b2e9
Lorena Souza <lcsouza@thoughtworks.com>
2018-03-03 23:25:18 -0300

    Corrige os erros

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,6 @@ import { withHistoryPropTypes } from './sharedPropTypes';
 const Nav = () => (
     <ul className='right hide-on-med-and-down'>
       <li><a name="sobre" href="/sobre">Sobre</a></li>
-      <li><a name="bla" href="/bla">Bla</a></li>
-      <li><a name="ta" href="/ta">Ta</a></li>
-      <li><a name="kl" href="/kl">Kl</a></li>
     </ul>
 );
 

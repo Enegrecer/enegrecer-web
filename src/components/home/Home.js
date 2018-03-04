5895dbb724319bdb8998afda801b96dd76e9285f
Lorena Souza <lcsouza@thoughtworks.com>
2018-03-03 21:40:44 -0300

    Cria menu como test

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Logo from './Logo';
-import Nav from './Nav';
-import Topbar from './Topbar';
-import Botao from './Botao';
-import Footer from './Footer';
-import Social from './Social';
-import { withHistoryPropTypes } from './sharedPropTypes';
-import './css/home.css';
-
+import Logo from './../Logo';
+import Nav from './../Nav';
+import Topbar from './../Topbar';
+import Botao from './../Botao';
+import Footer from './../Footer';
+import Social from './../Social';
+import { withHistoryPropTypes } from './../sharedPropTypes';
+import './home.css';
 
 const networks = [
   { title: 'Facebook', href: '#', icon: 'facebook-square' },
@@ -17,36 +16,14 @@ const networks = [
 
 const Home = props => (
   <div>
-    <div id="banner">
-      <div id="banner__blur" />
-      <div className="container">
-        <Topbar
-          leftComponent={<Logo />}
-          rightComponent={
-            <Nav history={
-              props.history // eslint-disable-line react/prop-types
-            }
-            />}
-        />
-
-        <div className="row">
-          <div className="col-md-4">
-            <div className="en-subtitle--primary">LOREM IPSUM SIT</div>
-            <div className="en-title en-title--light en-title--marker">LOREM IPSUM CONSECTETUR</div>
-            <p id="en-banner__description" className="en-text--light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <Botao
-              name="denuncia"
-              onClick={
-                () => props.history.push('/painel/denuncias/nova') // eslint-disable-line react/prop-types
-              }
-              colored
-            >Denunciar Racismo</Botao>
-          </div>
-        </div>
-      </div>
+    <Topbar
+        leftComponent={<Logo />}
+        rightComponent={<Nav history={props.history} />} // eslint-disable-line react/prop-types
+    />
+    <div className="btn-denuncia">
+      <Botao name="denuncia"
+        onClick={() => props.history.push('/painel/denuncias/nova')} // eslint-disable-line react/prop-types
+          colored>Denunciar Racismo</Botao>
     </div>
 
     <Footer>
--- src/components/home/Nav.js
+++ src/components/home/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { withHistoryPropTypes } from './sharedPropTypes';
 
-
 const Nav = () => (
-  <nav className="en-nav">
-    <a className="en-extra-margin-right" name="sobre" href="/sobre">Sobre</a>
-  </nav>
+    <ul className='right hide-on-med-and-down'>
+      <li><a name="sobre" href="/sobre">Sobre</a></li>
+      <li><a name="bla" href="/bla">Bla</a></li>
+      <li><a name="ta" href="/ta">Ta</a></li>
+      <li><a name="kl" href="/kl">Kl</a></li>
+    </ul>
 );
 
 Nav.propTypes = withHistoryPropTypes();

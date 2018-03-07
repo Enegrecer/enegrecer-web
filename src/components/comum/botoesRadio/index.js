a871569e81a3d9befe48130e43344c0d8b9e38d1
Daniel Faria <lenin@stack.com>
2018-03-07 10:57:11 -0300

    :art: Componente de radio de botoes adicionados

---
+++ src/components/comum/botoesRadio/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function BotoesRadio(props) {
+    return (
+      <div className={props.classes}>
+        { props.botoes.map(
+          botaoRadio =>
+            (<p key={botaoRadio.id}>
+              <input name={props.id} type="radio" id={botaoRadio.id} />
+              <label htmlFor={botaoRadio.id}>{botaoRadio.valor}</label>
+            </p>
+            )
+        )
+        }
+      </div>
+    )
+  }
+  
+  BotoesRadio.propTypes = {
+    classes: PropTypes.string,
+    id: PropTypes.string,
+    botoes: PropTypes.arrayOf(PropTypes.object)
+  };
+  
+  BotoesRadio.defaultProps = {
+    classes: '',
+    id: '',
+    botoes: []
+  };
+  

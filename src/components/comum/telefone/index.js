99878742c95530e681c2fb68d77ea4564fd05285
Daniel Faria <lenin@stack.com>
2018-03-09 14:51:35 -0300

    :art: Componente telefone com objeto destrutor

--- src/components/comum/telefone/index.js
+++ src/components/comum/telefone/index.js
@@ -2,34 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import InputMask from 'react-input-mask';
 
-export default function Telefone({ handleChange, ...props }) {
-  return (
+const telefone = ({ handleChange, id, value, label }) => (
     <div className="input-field col s6">
       <InputMask
-        {...props}
         type="text"
-        name={props.id}
-        id={props.id}
-        onChange={e => handleChange(e.target.value, props.id)}
-        value={props.value}
+        name={id}
+        id={id}
+        onChange={e => handleChange(e.target.value, id)}
+        value={value}
         mask="(99) 99999-9999"
         maskChar=" "
       />
-      <label htmlFor={props.id}>{props.label}</label>
+      <label htmlFor={id}>{label}</label>
     </div>
-  )
-}
+  );
 
-Telefone.propTypes = {
+telefone.propTypes = {
   id: PropTypes.string,
   label: PropTypes.string,
   handleChange: PropTypes.func,
   value: PropTypes.string
 };
 
-Telefone.defaultProps = {
+telefone.defaultProps = {
   id: '',
   label: '',
   handleChange: () => {},
   value: PropTypes.string
 };
+
+export default telefone;

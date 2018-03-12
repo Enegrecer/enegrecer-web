e5441eea2f42b9f432fe289b99905a8442d36cbd
Daniel Faria <lenin@stack.com>
2018-03-12 14:35:38 -0300

    Remoção de espaço em branco de sagas/denuncias

--- src/sagas/denuncias.js
+++ src/sagas/denuncias.js
@@ -68,7 +68,6 @@ export function* handleCriarDenunciaRequisicao() {
   while (true) {
     const acao = yield take(CRIAR_DENUNCIA_REQUISICAO);
     const idDenuncia = yield call(criarDenuncia, acao);
-
     yield put(criarDenunciaSucesso(idDenuncia));
   }
 }

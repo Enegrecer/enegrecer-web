9df2326a1d7c76406c0194232219d939a228f9d3
Erika Silva <essilva@thoughtworks.com>
2018-03-19 18:49:27 -0300

    Altera componente de radio button para ser controlado pelo redux.

--- src/components/comum/botoesRadio/index.js
+++ src/components/comum/botoesRadio/index.js
@@ -1,31 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
 
-const botoesRadio = ({ classes, botoes, id }) => (
+const renderizaInputComum = ({ classes, id, valor }) => (
   <div className={classes}>
-    { botoes.map(
-      botaoRadio =>
-        (<p key={botaoRadio.id}>
-          <input name={id} type="radio" id={botaoRadio.id} />
-          <label htmlFor={botaoRadio.id}>{botaoRadio.valor}</label>
-        </p>
-        )
-    )
-    }
+    <p key={id}>
+      <input name={id} type="radio" id={id} />
+      <label htmlFor={id}>{valor}</label>
+    </p>
   </div>
-);
+)
+
+const botoesRadio = ({ classes, botoes, state }) => {
+  if (state) {
+    return (
+      <div className={classes}>
+        {botoes.map(botaoRadio => (
+          <p key={botaoRadio.valor}>
+            <Field
+              id={botaoRadio.valor}
+              name={state}
+              component="input"
+              type="radio"
+              value={botaoRadio.valor}
+            />
+            <label htmlFor={botaoRadio.valor}>{botaoRadio.label}</label>
+          </p>
+        ))}
+      </div>
+    )
+  }
+  return (
+    <div className={classes}>
+      {botoes.map(botaoRadio => (renderizaInputComum(classes, botaoRadio.id, botaoRadio.valor)))}
+    </div>
+  )
+}
 
 botoesRadio.propTypes = {
   classes: PropTypes.string,
-  id: PropTypes.string,
-  botoes: PropTypes.arrayOf(PropTypes.object)
+  botoes: PropTypes.arrayOf(PropTypes.object),
+  state: PropTypes.string
 };
 
 botoesRadio.defaultProps = {
   classes: '',
   id: '',
-  botoes: []
+  botoes: [],
+  state: undefined
 };
 
-export default botoesRadio;
+renderizaInputComum.propTypes = {
+  classes: PropTypes.string,
+  valor: PropTypes.string,
+  id: PropTypes.string
+}
 
+renderizaInputComum.defaultProps = {
+  classes: '',
+  id: '',
+  valor: ''
+}
+
+export default botoesRadio;

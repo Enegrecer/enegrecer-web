558af4ef0e02f96972e6a774190a86e73b277500
Erika Silva <essilva@thoughtworks.com>
2018-03-21 16:59:21 -0300

    atualiza teste para fazer apenas o shallow e snapshot

--- src/components/comum/botoesRadio/BotoesRadio.test.js
+++ src/components/comum/botoesRadio/BotoesRadio.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import BotoesRadio from './index';
 
@@ -10,7 +10,7 @@ describe('Botões Rádio', () => {
 
   it('deve possuir estrutura definida ', () => {
     const onChange = jest.fn();
-    const botoesRadio = mount(<BotoesRadio
+    const botoesRadio = shallow(<BotoesRadio
       state={'idOcorrencia'}
       classes={'col s3'}
       id={'id'}
--- src/components/denuncias/DetalhamentoDenuncia.test.js
+++ src/components/denuncias/DetalhamentoDenuncia.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import DetalhamentoDenuncia from './DetalhamentoDenuncia';
 
 describe('DetalhamentoDenuncia Component', () => {
   it('deve possuir estrutura definida ', () => {
-    const detalhamentoDenuncia = mount(<DetalhamentoDenuncia />);
+    const detalhamentoDenuncia = shallow(<DetalhamentoDenuncia />);
     const detalhamentoDenunciaJson = toJson(detalhamentoDenuncia);
     expect(detalhamentoDenunciaJson).toMatchSnapshot();
   });

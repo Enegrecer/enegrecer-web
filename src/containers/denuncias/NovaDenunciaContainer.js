513d98148049f517a3587293a6a68a49255791a1
Erika Silva <essilva@thoughtworks.com>
2018-03-21 16:51:49 -0300

    remove código comentado

--- src/containers/denuncias/NovaDenunciaContainer.js
+++ src/containers/denuncias/NovaDenunciaContainer.js
@@ -1,4 +1,3 @@
-import FormCode from './FormCode';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
@@ -10,106 +9,8 @@ import {
 import NovaDenunciaForm from '../../components/denuncias/NovaDenunciaForm';
 import { validaDenuncia } from './validaDenuncia';
 import * as Tela from '../../utils/materializeCSS'
-
-
 
 class NovaDenunciaContainer extends React.Component {
-  submit = (values) => {
-    alert("submitted");
-    console.log(values);
-  }
-
-  componentDidMount() {
-    Tela.iniciaCamposMaterialize();
-  }
-
-  componentWillUnmount() {
-    if (this.props.denunciaCadastradaComSucesso) {
-      this.props.limpaEstadoUltimaDencunciaCadastrada();
-    }
-  }
-
-  onPressSaveButton = () => {
-    const mensagemError = validaDenuncia(this.state.vitima);
-    if (mensagemError === undefined) {
-      this.props.criarDenunciaRequisicao({
-        ...this.state
-      });
-    } else {
-      alert(mensagemError);
-    }
-  }
-
-  render() {
-    if (this.props.denunciaCadastradaComSucesso) {
-      return <Redirect to="/painel/proximosPassos" />;
-    }
-    return (
-      <div className="container">
-      <NovaDenunciaForm
-        onSubmit={this.onPressSaveButton}
-        alterarDenunciaForm={this.adicionarDenunciaNoForm}
-      />
-    </div>
-    );
-  }
-}
-
-NovaDenunciaContainer.propTypes = {
-  criarDenunciaRequisicao: PropTypes.func.isRequired,
-  limpaEstadoUltimaDencunciaCadastrada: PropTypes.func.isRequired,
-  denunciaCadastradaComSucesso: PropTypes.bool.isRequired
-};
-
-NovaDenunciaContainer.defaultProps = {
-  criarDenunciaRequisicao: () => {},
-  denunciaCadastradaComSucesso: false,
-  limpaEstadoUltimaDencunciaCadastrada: () => {},
-};
-
-
-const mapStateToProps = state => ({
-  denunciante: state.auth ? state.auth.user.uid : undefined,
-  denunciaCadastradaComSucesso: state.denunciaComSucessoReducer.denunciaCadastradaComSucesso
-});
-
-const mapDispatchToProps = dispatch => bindActionCreators({
-  criarDenunciaRequisicao,
-  limpaEstadoUltimaDencunciaCadastrada
-}, dispatch);
-
-const reduxNovaDenuncia = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(NovaDenunciaContainer);
-
-export default reduxNovaDenuncia;
-
-/*
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import {
-  criarDenunciaRequisicao, limpaEstadoUltimaDencunciaCadastrada
-} from '../../actions/criarDenunciaActions';
-import NovaDenunciaForm from '../../components/denuncias/NovaDenunciaForm';
-import { validaDenuncia } from './validaDenuncia';
-import * as Tela from '../../utils/materializeCSS'
-
-export class NovaDenunciaContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.onPressSaveButton = this.onPressSaveButton.bind(this);
-    this.adicionarDenunciaNoForm = this.adicionarDenunciaNoForm.bind(this);
-    this.state = {
-      vitima: null,
-      denunciante: null,
-      testemunha: null
-    };
-  }
-
   componentDidMount() {
     Tela.iniciaCamposMaterialize();
   }
@@ -131,21 +32,17 @@ export class NovaDenunciaContainer extends Component {
     }
   }
 
-  adicionarDenunciaNoForm(denuncia) {
-    this.setState({
-      ...denuncia,
-    })
-  }
-
   render() {
     if (this.props.denunciaCadastradaComSucesso) {
       return <Redirect to="/painel/proximosPassos" />;
     }
     return (
-      <NovaDenunciaForm
-        salvarDenuncia={this.onPressSaveButton}
-        alterarDenunciaForm={this.adicionarDenunciaNoForm}
-      />
+      <div className="container">
+        <NovaDenunciaForm
+          onSubmit={this.onPressSaveButton}
+          alterarDenunciaForm={this.adicionarDenunciaNoForm}
+        />
+      </div>
     );
   }
 }
@@ -179,4 +76,3 @@ const reduxNovaDenuncia = connect(
 )(NovaDenunciaContainer);
 
 export default reduxNovaDenuncia;
-*/

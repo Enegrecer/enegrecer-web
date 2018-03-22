0f2c234f51f21e469bcfac8beb4ab25aa031415f
Erika Soares da Silva <erikasons@gmail.com>
2018-03-22 09:01:04 -0300

    remove método de onChange

--- src/components/comum/caracteristica/index.js
+++ src/components/comum/caracteristica/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import CampoTexto from '../campoTexto';
 import { CLASSES_TEXTAREA, CLASSES_DIV_INPUT } from '../../../components/denuncias/ConstantesCss';
 
-const caracteristica = ({ id, onChange, label, validacoes, state }) => (<CampoTexto
+const caracteristica = ({ id, label, validacoes, state }) => (<CampoTexto
   id={id}
   label={label}
   maxLen={255}
@@ -11,20 +11,17 @@ const caracteristica = ({ id, onChange, label, validacoes, state }) => (<CampoTe
   inputClasse={CLASSES_TEXTAREA}
   divClasse={`${CLASSES_DIV_INPUT} col s12`}
   placeholder={'Ex.: Era uma mulher negra, com aproximadamente 40 anos, magra, alta com cabelo curto...'}
-  onChange={onChange}
   validacoes={validacoes}
   state={state}
 />);
 
 caracteristica.propTypes = {
   id: PropTypes.string,
-  onChange: PropTypes.func,
   label: PropTypes.string
 };
 
 caracteristica.defaultProps = {
   id: '',
-  onChange: () => {},
   label: ''
 };
 

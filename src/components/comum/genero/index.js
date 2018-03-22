ebb08e996bce5467f871860d7ec9b01c3eb065a9
Erika Soares da Silva <erikasons@gmail.com>
2018-03-22 09:01:40 -0300

    inclui state no campo gênero.

--- src/components/comum/genero/index.js
+++ src/components/comum/genero/index.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import CampoTexto from '../campoTexto';
 import { CLASSES_INPUT } from '../../../components/denuncias/ConstantesCss';
 
-const genero = ({ id, divClasse, onChange }) => (
+const genero = ({ id, state, divClasse }) => (
   <CampoTexto
+    state={state}
     id={id}
     label={'Gênero (máximo de 15 caracteres)'}
     maxLen={15}
     divClasse={divClasse}
     inputClasse={CLASSES_INPUT}
-    onChange={onChange}
     placeholder={'Ex.: Feminino, Masculino, Não Binário...'}
     type={'text'}
   />
@@ -19,13 +19,13 @@ const genero = ({ id, divClasse, onChange }) => (
 genero.propTypes = {
   id: PropTypes.string,
   divClasse: PropTypes.string,
-  onChange: PropTypes.func
+  state: PropTypes.string,
 };
 
 genero.defaultProps = {
   id: '',
   divClasse: '',
-  onChange: () => { }
+  state: '',
 };
 
 export default genero;

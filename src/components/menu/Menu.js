7f0dfa13d7b2f0c2f509b2c9f3c40e7fe9c383dd
gabrielavmattos <gmattos@thoughtworks.com>
2018-03-24 16:30:10 -0300

    [@gabrielavmattos, @edlainezamora, @lauradebella, @paulogama, @jfmedeiros1820] Incluindo link para a home.

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,19 +4,24 @@ import logo from '../../imagens/logo.svg';
 import './menu.css';
 
 export default class Menu extends Component {
+  componentWillMount() {
+  }
+
   render() {
     return (
       <div className="menu">
         <div className="logo">
-          <img src={logo} alt="Logo Verdade Seja Dita" />
+          <Link to="/">
+            <img src={logo} alt="Logo Verdade Seja Dita" />
+          </Link>
         </div>
         <div className="links">
           <Link to="/sobre">SOBRE</Link>
           <Link to="/depoisdenuncia">E DEPOIS DA DENÚNCIA?</Link>
-          <Link to="/coletivo">O COLETIVO ENEGRECER</Link>
+          <Link to="/coletivo-enegrecer">O COLETIVO ENEGRECER</Link>
           <Link to="/denunciar">DENUNCIAR</Link>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import HomePage from '../components/home/Home';
-import Painel from './Painel';
-import NovaDenuncia from '../containers/denuncias/NovaDenunciaContainer';
-import ProximosPassos from '../components/denuncias/proximosPassos';
 import Menu from '../components/menu/Menu';
+import ColetivoEnegrecer from '../components/coletivoEnegrecer/ColetivoEnegrecer';
 
 const Routes = () => (
   <BrowserRouter>
@@ -12,6 +10,7 @@ const Routes = () => (
       <Route path="/" component={Menu} />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/coletivo-enegrecer" component={ColetivoEnegrecer} />
       </Switch>
     </div>
   </BrowserRouter>

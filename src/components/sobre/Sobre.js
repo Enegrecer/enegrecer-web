0b2da7dc8a83ac1b27dc16b6866e0e18d707a6ab
Caique Peixoto <cpeixoto@thoughtworks.com>
2018-03-24 20:16:06 -0300

    Corrige estilo na página Sobre e no componente Colaborador.

--- src/components/sobre/Sobre.js
+++ src/components/sobre/Sobre.js
@@ -8,7 +8,9 @@ const Sobre = () => (
     <div className="col s6 about_img">
     </div>
     <div className="col s6 about_section">
-      <h1 id="sobre">Sobre Verdade Seja Dita</h1>
+      <h1 id="sobre">Sobre</h1>
+      <h1>Verdade Seja</h1>
+      <h1>Dita</h1>
 
       <p>Somos mulheres e homens e definimos a nossa atuação na defesa e promoção dos direitos da população negra e na construção de uma sociedade onde os valores de justiça, equidade e solidariedade são fundamentais. Consideramos o racismo e a discriminação racial como mecanismos combinados que estruturam as relações sociais, cujos resultados diretos se expressam em prejuízos para as negras e negros. O racista brasileiro não tem nada de cordial e amistoso como muitos imaginam e sustentam. Pelo contrário.  Nosso objetivo é criar e aplicar novas tecnologias para a luta políticas dos negros e negras contra o racismo; produzindo sobre o contexto atual das questões envolvendo os nossos direitos.</p>
     </div>
--- src/components/sobre/componentes/Colaborador.js
+++ src/components/sobre/componentes/Colaborador.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Colaborador = props => (
   <div className="col s4 person">
     <div className="content">
-      <img src={ props.foto } />
+      <img alt={ 'Foto de ' + props.nome } src={ props.foto } />
       <div>
         <h5>{ props.nome }</h5>
         <p>{ props.papel }</p>

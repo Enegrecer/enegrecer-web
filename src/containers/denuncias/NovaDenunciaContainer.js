9390df329897a3f9b1d439e10d77864311916a45
Erika Soares da Silva <erikasons@gmail.com>
2018-03-24 22:34:44 -0300

    realiza o bind da função onPressSaveButton e atualiza

--- src/containers/denuncias/NovaDenunciaContainer.js
+++ src/containers/denuncias/NovaDenunciaContainer.js
@@ -9,13 +9,17 @@ import {
 import NovaDenunciaForm from '../../components/denuncias/NovaDenunciaForm';
 
 class NovaDenunciaContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onPressSaveButton = this.onPressSaveButton.bind(this);
+  }
   componentWillUnmount() {
     if (this.props.denunciaCadastradaComSucesso) {
       this.props.limpaEstadoUltimaDencunciaCadastrada();
     }
   }
 
-  onPressSaveButton =() => {
+  onPressSaveButton() {
     this.props.criarDenunciaRequisicao(
       this.props.formDenuncia.values
     );
@@ -26,11 +30,26 @@ class NovaDenunciaContainer extends React.Component {
       return <Redirect to="/painel/proximosPassos" />;
     }
     return (
-      <div className="container">
-        <NovaDenunciaForm
-          onSubmit={this.onPressSaveButton}
-          alterarDenunciaForm={this.adicionarDenunciaNoForm}
-        />
+      <div className="row">
+        <div className="col s2 colspan-ignore-padding">
+          <div className="left-menu">
+            <div className="page-bar">
+              <h5>Formulário de Denúncia</h5>
+            </div>
+            <div className="menu-items">
+              <ul>
+                <li>Dados do Denunciante</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col s10 colspan-ignore-padding">
+          <NovaDenunciaForm
+            onSubmit={this.onPressSaveButton}
+            alterarDenunciaForm={this.adicionarDenunciaNoForm}
+          />
+        </div>
       </div>
     );
   }
@@ -39,13 +58,15 @@ class NovaDenunciaContainer extends React.Component {
 NovaDenunciaContainer.propTypes = {
   criarDenunciaRequisicao: PropTypes.func.isRequired,
   limpaEstadoUltimaDencunciaCadastrada: PropTypes.func.isRequired,
-  denunciaCadastradaComSucesso: PropTypes.bool.isRequired
+  denunciaCadastradaComSucesso: PropTypes.bool.isRequired,
+  formDenuncia: PropTypes.string
 };
 
 NovaDenunciaContainer.defaultProps = {
   criarDenunciaRequisicao: () => {},
   denunciaCadastradaComSucesso: false,
   limpaEstadoUltimaDencunciaCadastrada: () => {},
+  formDenuncia: ''
 };
 
 const mapStateToProps = state => ({

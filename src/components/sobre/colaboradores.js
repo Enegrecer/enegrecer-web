3aade5441e38597dfe6e8189ab83c71bb711f286
Leandro <leandrovicente.info@gmail.com>
2018-03-29 16:26:20 -0300

    Migra o armazenamento das fotos das pessoas contribuintes para o firebase storage

--- src/components/sobre/colaboradores.js
+++ src/components/sobre/colaboradores.js
@@ -1,178 +1,174 @@
-function carregarFoto(nomeArquivo) {
-  return require(`../../imagens/pessoas/${nomeArquivo}`);
-}
-
 export function colaboradores() {
   return [
     {
       nome: 'Alisson Pereira',
       papel: 'QA',
-      foto: carregarFoto('alisson-pereira.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Falisson-pereira.jpg?alt=media&token=f317aa99-e343-474b-838f-c3ecae379e17',
     },
     {
       nome: 'Ayrton Araújo',
       papel: 'DEV',
-      foto: carregarFoto('ayrton-araujo.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fayrton-araujo.jpg?alt=media&token=96c23804-c0f5-4efd-b6cc-906b65fed757',
     },
     {
       nome: 'Bárbara Sanches',
       papel: 'BA',
-      foto: carregarFoto('barbara-sanches.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fbarbara-sanches.jpg?alt=media&token=4ef955c0-55ea-4bf1-99d6-0ad89d5907fd',
     },
     {
       nome: 'Bárbara Tomaz',
       papel: 'XD',
-      foto: carregarFoto('barbara-tomaz.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fbarbara-tomaz.jpg?alt=media&token=c350dba2-eb28-46ce-a867-48d398a02d17',
     },
     {
       nome: 'Caique Peixoto',
       papel: 'DEV',
-      foto: carregarFoto('caique-peixoto.jpeg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fcaique-peixoto.jpeg?alt=media&token=787a64be-811b-473b-a019-6dc95a2d5006',
     },
     {
       nome: 'Carine Monteiro',
       papel: 'DEVa',
-      foto: carregarFoto('carine-monteiro.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fcarine-monteiro.jpg?alt=media&token=a5485e22-02a9-4ba9-8619-672ad8d89a10',
     },
     {
       nome: 'Daniel Faria',
       papel: 'DEV',
-      foto: carregarFoto('daniel-faria.png'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fdaniel-faria.png?alt=media&token=a8eb3e73-ac03-41d4-86d4-4533528a27eb',
     },
     {
       nome: 'Débora Beda',
       papel: 'DEVa',
-      foto: carregarFoto('debora-beda.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fdebora-beda.jpg?alt=media&token=d5fc5cea-0f07-4cf6-8d8f-8ac00c258e46',
     },
     {
       nome: 'Edlaine Zamora',
       papel: 'DEVa',
-      foto: carregarFoto('edlaine-zamora.jpeg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fedlaine-zamora.jpeg?alt=media&token=49355897-2c37-4859-a82a-fc2220d29040',
     },
     {
       nome: 'Egileu Trajano',
       papel: 'QA',
-      foto: carregarFoto('egileu-trajano.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fegileu-trajano.jpg?alt=media&token=8b5dabee-9ba9-4eb3-a158-7a65c60db0cb',
     },
     {
       nome: 'Erika Silva',
       papel: 'DEVa',
-      foto: carregarFoto('erika-silva.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Ferika-silva.jpg?alt=media&token=adf0ebc8-cf6a-4967-b42c-a17a4383d567',
     },
     {
       nome: 'Gabriel Pereira',
       papel: 'DEV',
-      foto: carregarFoto('gabriel-pereira.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fgabriel-pereira.jpg?alt=media&token=d41141ab-40d8-4789-b193-102825ce55ad',
     },
     {
       nome: 'Gabriela Mattos',
       papel: 'DEVa',
-      foto: carregarFoto('gabriela-mattos.png'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fgabriela-mattos.png?alt=media&token=615c00dc-1e79-40e5-856c-6262178fa03e',
     },
     {
       nome: 'Izael Effemberg',
       papel: 'DEV',
-      foto: carregarFoto('izael-effemberg.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fizael-effemberg.jpg?alt=media&token=f6478b20-9702-485b-9282-446eafe5662a',
     },
     {
       nome: 'João Moreira',
       papel: 'DEV',
-      foto: carregarFoto('joao-moreira.jpeg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fjoao-moreira.jpeg?alt=media&token=81fab300-2e6c-4155-a994-3d002288406b',
     },
     {
       nome: 'Kamila Serafim',
       papel: 'QA',
-      foto: carregarFoto('kamila-serafim.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fkamila-serafim.jpg?alt=media&token=23af266e-f6fe-4979-8a97-a5a697cd9135',
     },
     {
       nome: 'Karla Falcão',
       papel: 'DEVa',
-      foto: carregarFoto('karla-falcao.jpeg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fkarla-falcao.jpeg?alt=media&token=d84ff7ad-e2ce-43c1-ad24-1a9df2c6b142',
     },
     {
       nome: 'Laura de Bella',
       papel: 'DEVa',
-      foto: carregarFoto('laura-bella.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Flaura-bella.jpg?alt=media&token=842709e1-04e5-46ce-b1fd-491cacfdbafd',
     },
     {
       nome: 'Leandro Vicente',
       papel: 'DEV',
-      foto: carregarFoto('leandro-vicente.jpeg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fleandro-vicente.jpeg?alt=media&token=ec9f54e6-c59d-4c81-8c23-8e93f24a82e7',
     },
     {
       nome: 'Lorena Souza',
       papel: 'DEVa',
-      foto: carregarFoto('lorena-souza.png'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Florena-souza.png?alt=media&token=68743873-9ea1-4ae8-b5c8-7baf94a2c214',
     },
     {
       nome: 'Luciana Montenegro',
       papel: 'DEVa',
-      foto: carregarFoto('luciana-montenegro.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fluciana-montenegro.jpg?alt=media&token=b9a49a6b-de78-4837-a480-d39cf3115f6c',
     },
     {
       nome: 'Marcella Souza',
       papel: 'DEVa',
-      foto: carregarFoto('marcella-souza.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fmarcella-souza.jpg?alt=media&token=7b38e3b3-e993-461d-a0c9-93f4b7a96b99',
     },
     {
       nome: 'Mariane Ferroni',
       papel: 'BA',
-      foto: carregarFoto('mariane-ferroni.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fmariane-ferroni.jpg?alt=media&token=a0c497c5-eaf8-452a-a20c-dc43df4ce246',
     },
     {
       nome: 'Mila Orrico',
       papel: 'BA',
-      foto: carregarFoto('mila-orrico.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fmila-orrico.jpg?alt=media&token=f09a3af0-30a7-45df-97a4-1d3c6e652b79',
     },
     {
       nome: 'Nathalia Oliveira',
       papel: 'PM',
-      foto: carregarFoto('nathalia-oliveira.png'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fnathalia-oliveira.png?alt=media&token=0199df2c-5e21-448b-a562-5468c896aaad',
     },
     {
       nome: 'Nayara Souza',
       papel: 'DEVa',
-      foto: carregarFoto('nayara-souza.png'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fnayara-souza.png?alt=media&token=7f38d6a0-8903-45bf-9846-929795b60102',
     },
     {
       nome: 'Paulo Gama',
       papel: 'DEV',
-      foto: carregarFoto('paulo-gama.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fpaulo-gama.jpg?alt=media&token=6b9e8cba-53cc-4c50-84b9-cad489fc37be',
     },
     {
       nome: 'Priscila Rocha',
       papel: 'PM',
-      foto: carregarFoto('priscila-rocha.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fpriscila-rocha.jpg?alt=media&token=04d3c50b-73f2-4bb1-b108-84286ab104d3',
     },
     {
       nome: 'Rafaella Souza',
       papel: 'DEVa',
-      foto: carregarFoto('rafaella-souza.jpeg')
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Frafaella-souza.jpeg?alt=media&token=68d28c4d-ef4b-4306-9382-46e8d61a262e',
     },
     {
       nome: 'Rosangela Mendes',
       papel: 'DEVa',
-      foto: carregarFoto('rosangela-mendes.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Frosangela-mendes.jpg?alt=media&token=42c77d11-e328-4f62-90d5-a8ca6f908974',
     },
     {
       nome: 'Roselma Mendes',
       papel: 'DEVa',
-      foto: carregarFoto('roselma-mendes.png'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Froselma-mendes.png?alt=media&token=18d57eac-0d6d-4e04-a5d2-0fa3af76232e',
     },
     {
       nome: 'Sérgio Luis',
       papel: 'BA',
-      foto: carregarFoto('sergio-luis.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fsergio-luis.jpg?alt=media&token=5f03b54b-5b15-4cbb-8bda-b20cf9750e33',
     },
     {
       nome: 'Thais Lopes',
       papel: 'DEVa',
-      foto: carregarFoto('thais-lopes.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Fthais-lopes.jpg?alt=media&token=6733cfb8-d256-4dfb-9673-d974683cac4f',
     },
     {
       nome: 'Tiago Megale',
       papel: 'BA',
-      foto: carregarFoto('tiago-megale.jpg'),
+      foto: 'https://firebasestorage.googleapis.com/v0/b/enegrecer-e37b3.appspot.com/o/fotos%2Fpessoas%2Ftiago-megale.jpg?alt=media&token=b50b0142-d9d4-42ce-ba81-d2aaa6635cda',
     }
   ];
 }

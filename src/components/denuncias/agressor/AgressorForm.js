366a44c866173529d6a6dfd38f593d7ccce27caa
Erika Soares da Silva <erikasons@gmail.com>
2018-04-02 10:17:20 -0300

    inclui validações ao form de denúncia.

--- src/components/denuncias/agressor/AgressorForm.js
+++ src/components/denuncias/agressor/AgressorForm.js
@@ -4,41 +4,49 @@ import CampoTexto from '../../comum/campoTexto';
 import Genero from '../../comum/genero';
 import Data from '../../comum/data';
 import ComboboxEstado from '../../comum/comboboxEstado';
+import Combobox from '../../comum/combobox';
 import * as ConstantesCSS from '../ConstantesCss';
 import Raca from '../../comum/comboboxRaca';
+import { campoObrigatorio } from '../../comum/validacoes';
 
 const AgressorForm = () => (
   <div>
     <div className="row">
       <Nome state={'nomeAgressor'} id={'nome-agressor'} />
-      <CampoTexto
-        state={'cidadeAgressor'}
-        id={'cidade-agressor'}
-        label={'Cidade'}
-        maxLen={40}
-        placeholder={''}
-        type={'text'}
-        divClasse={'input-field col s6'}
-      />
-    </div>
-
-    <div className="row">
       <Genero
         state={'generoAgressor'}
         id={'genero-agressor'}
         divClasse={'col s6'}
       />
+    </div>
 
+    <div className="row">
+      <ComboboxEstado
+        state={'estadoAgressor'}
+        id={'estado-agressor'}
+        divClasse={'col s6'}
+      />
+      <Combobox
+        id={'cidade-agressor'}
+        state={'cidadeAgressor'}
+        itens={['Belo Horizonte']}
+        divClasse={'col s6'}
+        label={'Cidade *'}
+      />
+    </div>
+
+    <div className="row">
       <Data
         state={'dataNascimentoAgressor'}
         id={'data-nascimento-agressor'}
         label={'Data de Nascimento'}
         divClasse={`${ConstantesCSS.CLASSES_DIV_INPUT} col s6`}
       />
+      <Raca id={'raca-agressor'} state={'racaAgressor'} divClasse={'col s6'} somenteRacasVitima={false} />
+
     </div>
 
     <div className="row">
-      <Raca id={'raca-agressor'} state={'racaAgressor'} divClasse={'col s6'} somenteRacasVitima={false} />
       <CampoTexto
         state={'bairroAgressor'}
         id={'bairro-agressor'}
@@ -59,12 +67,9 @@ const AgressorForm = () => (
         placeholder={''}
         type={'text'}
         divClasse={'input-field col s12'}
+        validacoes={[campoObrigatorio]}
       />
-      <ComboboxEstado
-        state={'estadoAgressor'}
-        id={'estado-agressor'}
-        classes={'col s6'}
-      />
+
     </div>
   </div>
 );

d737ddaeef2e9ba5170c09c556eed51bc018d8eb
Lorena Souza <lcsouza@thoughtworks.com>
2018-04-11 12:51:29 -0300

    [ISSUE-160] Ajusta lint

--- src/components/estatistica/Estatistica.js
+++ src/components/estatistica/Estatistica.js
@@ -12,15 +12,43 @@ const Estatistica = () => (
       base de dados acessível, transparente e atualizada. </p>
     </div>
 
-    <TipoDeEstatistica key="vermelho" id="vermelho" numero="2345" categoria="Racismo" />
+    <TipoDeEstatistica
+      key="vermelho"
+      id="vermelho"
+      numero="2345"
+      categoria="Racismo"
+    />
+
     <div id="rectangle2">
       <div id="rectangle21">
-        <TipoDeEstatistica key="amarelo" id="amarelo" numero="2345" categoria="Injúria Racial" />
-        <TipoDeEstatistica key="verde" id="verde" numero="60%" categoria="Vítimas Mulheres" />
+        <TipoDeEstatistica
+          key="amarelo"
+          id="amarelo"
+          numero="2345"
+          categoria="Injúria Racial"
+        />
+
+        <TipoDeEstatistica
+          key="verde"
+          id="verde"
+          numero="60%"
+          categoria="Vítimas Mulheres"
+        />
       </div>
       <div id="rectangle22">
-        <TipoDeEstatistica key="cinza" id="cinza" numero="23" categoria="Vítima até 26 anos" />
-        <TipoDeEstatistica key="cinzaClaro" id="cinzaClaro" numero="346" categoria="Total de Denúncia" />
+        <TipoDeEstatistica
+          key="cinza"
+          id="cinza"
+          numero="23"
+          categoria="Vítima até 26 anos"
+        />
+
+        <TipoDeEstatistica
+          key="cinzaClaro"
+          id="cinzaClaro"
+          numero="346"
+          categoria="Total de Denúncia"
+        />
       </div>
     </div>
   </div>
--- src/components/estatistica/TipoDeEstatistica.js
+++ src/components/estatistica/TipoDeEstatistica.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TipoDeEstatistica = props => (
-  <div id={ props.id }>
+  <div id={props.id}>
     <h3 className="numero">{ props.numero } </h3>
     <h6 className="categoria">{ props.categoria } </h6>
   </div>

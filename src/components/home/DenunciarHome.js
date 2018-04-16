2a25c395d41d5b05c67fd2e24cfb7c8de6381f91
Lorena Souza <lcsouza@thoughtworks.com>
2018-04-15 22:34:11 -0300

    [ISSUE-160] Cria a parte de denunciar na pagina da Home

--- src/components/home/DenunciarHome.js
+++ src/components/home/DenunciarHome.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './css/home.css';
+import img from '../../imagens/ayo-ogunseinde.png'
 
 const DenunciarHome = () => (
   <div className="denunciarHome">
-    alguma coisa.
+    <div className="parte1">
+      <img src={img}/>
+    </div>
+    <div className="parte2">
+    </div>
   </div>
 );
 

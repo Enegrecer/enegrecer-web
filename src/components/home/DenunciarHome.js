2196ba9278b14271181730f0f3586f19e1f90a2b
Lorena Souza <lcsouza@thoughtworks.com>
2018-04-15 21:30:10 -0300

    [ISSUE-160] Ajusta CSS

---
+++ src/components/home/DenunciarHome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/home.css';
+
+const DenunciarHome = () => (
+  <div className="denunciarHome">
+    alguma coisa.
+  </div>
+);
+
+export default DenunciarHome;

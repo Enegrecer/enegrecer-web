cf614ca86df371e1493c9bf4163d97d257b4ad3a
Lorena Souza <lcsouza@thoughtworks.com>
2018-04-15 23:17:26 -0300

    [ISSUE-160] Ajusta css

--- src/components/home/DenunciarHome.js
+++ src/components/home/DenunciarHome.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './css/home.css';
 import img from '../../imagens/ayo-ogunseinde.png'
+import { Link } from 'react-router-dom';
 
 const DenunciarHome = () => (
   <div className="denunciarHome">
-    <div className="parte1">
+    <div className="parte1 col s12 m6">
       <img src={img}/>
     </div>
-    <div className="parte2">
+    <div className="parte2 col s12 m6">
+    <h6> Nos ajude fazendo uma denúncia</h6>
+    <p> Gostaria de reportar uma situação em que
+    foi discriminado ou que presenciou? </p>
+    <Link className="denunciar" to="/denunciar">DENUNCIAR</Link>
     </div>
   </div>
 );

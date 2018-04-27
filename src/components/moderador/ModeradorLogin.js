bc0eabda5c95a074e922e200bd20df5a2dd7b4e4
Nayara Souza <nsouza@thoughtworks.com>
2018-04-27 01:23:25 -0300

    Traduz mensagens de validação do login moderador

--- src/components/moderador/ModeradorLogin.js
+++ src/components/moderador/ModeradorLogin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { auth } from '../../utils/firebaseUtils';
+
 import './moderador-login.css'
 
 const updateByPropertyName = (propertyName, value) => () => ({
@@ -19,6 +20,18 @@ export default class ModeradorLogin extends Component {
     this.state = { ...ESTADO_INICIAL }
   }
 
+  trataMensagemDeErro = (error) => {
+    if(error.code === 'auth/invalid-email') {
+      error.message = 'O email é inválido!'
+    } else if (error.code === 'auth/wrong-password') {
+      error.message = 'A senha está incorreta!'
+    } else if (error.code === 'auth/user-not-found') {
+      error.message = 'Usuário não encontrado!'
+    }
+
+    return error;
+  }
+
   onSubmit = (event) => {
     const {
       email,
@@ -31,6 +44,7 @@ export default class ModeradorLogin extends Component {
       
     })
     .catch(error => {
+      this.trataMensagemDeErro(error);
       this.setState(updateByPropertyName('error', error));
     });
 
@@ -56,18 +70,18 @@ export default class ModeradorLogin extends Component {
       <div className="row login_moderador">
         <form className="col s4 formulario_login_moderador" onSubmit={this.onSubmit}>
           <div className="input-field col s12">
-            <input id="login_moderador" type="text" class="validate" value={email} onChange={event => this.setState(updateByPropertyName('email', event.target.value))} />
-            <label for="login_moderador">Login Moderador</label>
+            <input id="login_moderador" type="text" className="validate" value={email} onChange={event => this.setState(updateByPropertyName('email', event.target.value))} />
+            <label htmlFor="login_moderador">Login Moderador</label>
           </div>
           <div className="input-field col s12">
             <input id="senha_moderador" type="password" className="validate" value={senha} onChange={event => this.setState(updateByPropertyName('senha', event.target.value))} />
-            <label for="senha_moderador">Senha Moderador</label>
+            <label htmlFor="senha_moderador">Senha Moderador</label>
           </div>
             <button className="btn waves-effect waves-light" disabled={isInvalid} type="submit" name="action" onClick={this.login}>
               Entrar
             </button>
-          { error && <p>{error.message}</p> }
-          
+
+            {error && <p>{error.message}</p>}
         </form>
       </div>
     );

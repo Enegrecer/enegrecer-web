a4fda729cd9d076afc118450238ea3341e37d577
Nayara Souza <nsouza@thoughtworks.com>
2018-04-28 10:42:46 -0300

    Adiciona reducer do login

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import denunciaComSucessoReducer from './denuncia/denunciaComSucessoReducer';
+import logadoComSucessoReducer from './login/logadoComSucessoReducer'
 
 export default combineReducers({
+  logadoComSucessoReducer,
   denunciaComSucessoReducer,
   form: formReducer
 });

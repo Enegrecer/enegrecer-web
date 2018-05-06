70fca3197344cd15ffff507b716c1bce5087041e
Nayara Souza <nsouza@thoughtworks.com>
2018-05-06 13:56:45 -0300

    Adiciona mock de componentes de menu e painel moderador

---
+++ src/components/moderador/menuModerador.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+var divStyle = {
+    backgroundColor: 'white',
+  };
+
+const MenuModerador = () => {
+    return(
+        <div style={divStyle}>
+            <h1>AQUI SERA CONSTRUIDO O MENU DO MODERADOR</h1>
+        </div>
+    )
+}
+
+export default MenuModerador;
---
+++ src/components/moderador/painelModerador.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+var divStyle = {
+    backgroundColor: 'white',
+};
+
+const PainelModerador = () => {
+    return(
+        <div style={divStyle}>
+            <h1>AQUI SERA CONSTRUIDA A PAGINA DO MODERADOR</h1>
+        </div>
+    )
+    
+}
+
+export default PainelModerador;

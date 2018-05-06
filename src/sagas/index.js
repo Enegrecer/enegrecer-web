ae7460eaee49eafd1037dd5ffd7c928d43cd89b5
Nayara Souza <nsouza@thoughtworks.com>
2018-05-06 14:13:46 -0300

    Remove saga do login

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,8 @@
 import { fork } from 'redux-saga/effects';
 import denuncia from './denuncia/denuncias';
+import login from './login/loginSaga';
 
 export default function* rootSaga() {
   yield fork(denuncia);
+  yield fork(login);
 }

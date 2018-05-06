07a1d51a268c54d110cf2f9d3cd7e98785a2cc02
Nayara Souza <nsouza@thoughtworks.com>
2018-05-06 14:11:44 -0300

    Remove reducer do login

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import denunciaComSucessoReducer from './denuncia/denunciaComSucessoReducer';
-import loginReducer from './login/loginReducer'
 
 export default combineReducers({
-  loginReducer,
   denunciaComSucessoReducer,
   form: formReducer
 });
+;

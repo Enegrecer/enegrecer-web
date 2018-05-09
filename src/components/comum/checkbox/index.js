9d61c6bd1f13f0aa981832eb1fb3a4b66ab3fe78
Erika Soares da Silva <erikasons@gmail.com>
2018-05-09 14:43:18 -0300

    adiciona propriedade para receber evento de onChange no checkbox.

--- src/components/comum/checkbox/index.js
+++ src/components/comum/checkbox/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
 
-const checkBox = ({ id, label, state, divClasse }) => (
+const checkBox = ({ id, label, state, divClasse, onChange }) => (
   <div className={divClasse}>
-    <Field id={id} name={state} component="input" type="checkbox" />
+    <Field id={id} name={state} component="input" type="checkbox" onChange={(event, newValue) => onChange(newValue)} />
     <label htmlFor={id}>{label}</label>
   </div>
 );
@@ -14,13 +14,15 @@ checkBox.propTypes = {
   label: PropTypes.string,
   state: PropTypes.string,
   divClasse: PropTypes.string,
+  onChange: PropTypes.func
 };
 
 checkBox.defaultProps = {
   id: '',
   label: '',
   state: '',
-  divClasse: ''
+  divClasse: '',
+  onChange: () => {}
 };
 
 export default checkBox;

d8286551a3767dbfce15c298b64b556dbf5e1775
Erika Soares da Silva <erikasons@gmail.com>
2018-05-09 14:55:59 -0300

    cria comportamento que copia dados do denunciante os da vítima.

--- src/components/denuncias/vitima/NovaVitimaForm.js
+++ src/components/denuncias/vitima/NovaVitimaForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CheckBox from '../../comum/checkbox';
 import * as ConstantesCSS from '../ConstantesCss';
 import Nome from '../../comum/nome';
@@ -8,52 +8,105 @@ import ComboboxRaca from './../../comum/comboboxRaca';
 import Data from '../../comum/data';
 import Endereco from '../../comum/endereco';
 
-const NovaVitimaForm = () => (
-  <div>
-    <div className="row">
-      <CheckBox
-        state={'souAVitima'}
-        divClasse={'col s12 m6 l6'}
-        id={'souAVitima'}
-        label={'Marque se você é a vítima'}
-      />
-    </div>
-    <div className="row">
-      <Nome id={'nome-vitima'} state={'nomeVitima'} />
-      <Data
-        state={'dataNascimentoVitima'}
-        id={'dataNascimento'}
-        label={'Data de Nascimento'}
-        divClasse={`${ConstantesCSS.CLASSES_DIV_INPUT} col s12 m6 l6`}
-      />
-    </div>
+class NovaVitimaForm extends Component {
+  geraObjetosParaEspelhar = () => {
+    const {
+      nomeDenunciante,
+      telefoneDenunciante,
+      estadoDenunciante,
+      cidadeDenunciante,
+      generoDenunciante,
+      dataNascimentoDenunciante,
+      racaDenunciante } = this.props.formDenuncia.values;
 
-    <Endereco estadoState={'estadoVitima'} cidadeState={'cidadeVitima'} />
+    return [
+      {
+        campo: 'nomeVitima',
+        valor: nomeDenunciante,
+      },
+      {
+        campo: 'telefoneVitima',
+        valor: telefoneDenunciante,
+      },
+      {
+        campo: 'estadoVitima',
+        valor: estadoDenunciante,
+      },
+      {
+        campo: 'dataNascimentoVitima',
+        valor: dataNascimentoDenunciante,
+      },
+      {
+        campo: 'cidadeVitima',
+        valor: cidadeDenunciante,
+      },
+      {
+        campo: 'generoVitima',
+        valor: generoDenunciante,
+      },
+      {
+        campo: 'racaVitima',
+        valor: racaDenunciante
+      }
+    ];
+  }
 
-    <div className="row">
-      <Telefone
-        divClasse={'input-field col s12 m6 l6'}
-        id="telefone"
-        state={'telefoneVitima'}
-        label={'Telefone'}
-      />
-    </div>
+  changeField = (foiVitima) => {
+    const objetosEspelho = this.geraObjetosParaEspelhar();
+    objetosEspelho.forEach((objeto) => {
+      this.props.changeFieldValue(objeto.campo, foiVitima ? objeto.valor : '');
+    });
+  }
 
-    <div className="row">
-      <Genero
-        state={'generoVitima'}
-        id={'vitima-genero'}
-        divClasse={'col s12 m6 l6'}
-      />
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <CheckBox
+            onChange={this.changeField}
+            state={'souAVitima'}
+            divClasse={'col s12 m6 l6'}
+            id={'souAVitima'}
+            label={'Marque se você é a vítima'}
+          />
+        </div>
+        <div className="row">
+          <Nome id={'nome-vitima'} state={'nomeVitima'} />
+          <Data
+            state={'dataNascimentoVitima'}
+            id={'dataNascimento'}
+            label={'Data de Nascimento'}
+            divClasse={`${ConstantesCSS.CLASSES_DIV_INPUT} col s12 m6 l6`}
+          />
+        </div>
 
-      <ComboboxRaca
-        state={'racaVitima'}
-        id={'raca'}
-        divClasse={'col s12 m6 l6'}
-        somenteRacasVitima
-      />
-    </div>
-  </div>
-);
+        <Endereco estadoState={'estadoVitima'} cidadeState={'cidadeVitima'} />
+
+        <div className="row">
+          <Telefone
+            divClasse={'input-field col s12 m6 l6'}
+            id="telefone"
+            state={'telefoneVitima'}
+            label={'Telefone'}
+          />
+        </div>
+
+        <div className="row">
+          <ComboboxRaca
+            state={'racaVitima'}
+            id={'raca'}
+            divClasse={'col s12 m6 l6'}
+            somenteRacasVitima
+          />
+          <Genero
+            state={'generoVitima'}
+            id={'vitima-genero'}
+            divClasse={'col s12 m6 l6'}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default NovaVitimaForm;

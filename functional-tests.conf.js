f0f02ccc03640fae425c15f2680f621c8cbaf436
Alisson Neves <apereira@thoughtworks.com>
2018-08-14 18:41:32 -0300

    Adicionando as informações da página e a implementação dos steps do cenário 'Realizar o cadastro de uma denúncia'

--- functional-tests.conf.js
+++ functional-tests.conf.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 /*
 Basic configuration to run your cucumber
 feature files and step definitions with protractor.
---
+++ src/tests/functional/pages/denuncia.js
@@ -0,0 +1,44 @@
+/*jshint esversion: 6 */
+
+let campoNomeDenunciante,
+  campoEmailDenunciante,
+  campoDescricaoAgressor,
+  botaoEnviarDenuncia,
+  timeout = 30000,
+  EC = protractor.ExpectedConditions;
+
+module.exports = {
+  init: () => {
+    campoNomeDenunciante = element(by.name('nomeDenunciante'));
+    campoEmailDenunciante = element(by.name('emailDenunciante'));
+    campoDescricaoAgressor = element(by.name('descricaoAgressor'));
+    botaoEnviarDenuncia = element(by.name('action'));
+  },
+
+  abrirPagina: () => {
+    const urlPage = 'http://localhost:3000/denunciar';
+    console.log(browser.getSession().value_.id_);
+    return browser.get(urlPage);
+  },
+
+  preencherCampoNomeDenunciante: (string) => {
+    browser.wait(EC.visibilityOf(campoNomeDenunciante), timeout);
+    return campoNomeDenunciante.sendKeys(string);
+  },
+
+  preencherCampoEmailDenunciante: (string) => {
+    browser.wait(EC.visibilityOf(campoEmailDenunciante), timeout);
+    return campoEmailDenunciante.sendKeys(string);
+  },
+
+  preencherCampoDescricaoAgressor: (string) => {
+    browser.wait(EC.visibilityOf(campoDescricaoAgressor), timeout);
+    return campoDescricaoAgressor.sendKeys(string);
+  },
+
+  salvarDenuncia: () => {
+    browser.wait(EC.elementToBeClickable(botaoEnviarDenuncia), timeout);
+    return botaoEnviarDenuncia.click();
+  }
+
+};

e1ffe235a336b5a1a6e50e2f836cd0bb8ee72a5b
Alisson Neves <apereira@thoughtworks.com>
2018-08-28 09:39:15 -0300

    Ajustar tipo de argumento para comparação no shell script

--- src/tests/functional/runSmokeTest.sh
+++ src/tests/functional/runSmokeTest.sh
@@ -6,28 +6,29 @@ echo Instalando o Protractor globalmente...
 
 echo Atualizando o servidor do Selenium...
 
-    if [$0 == true]
+    ra1 = $?;
+    if [ra1 = 0]
     then webdriver-manager update
     else exit $0
     fi
 
 echo Iniciando o servidor...
 
-    if [$1 == true]
+    if [$? = 0]
     then webdriver-manager start --detach
     else exit $1
     fi
 
 echo Executando os testes...
 
-    if [$2 == true]
+    if [$? = 0]
     then protractor functional-tests.conf.js
     else exit $2
     fi
 
 echo Parando o servidor...
 
-    if [$3 == true]
+    if [$? = 0]
     then webdriver-manager shutdown
     else exit $3
     fi

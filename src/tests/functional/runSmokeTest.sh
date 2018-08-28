6b450fa6af1ef3d3544d616efda16a2117cfa9dc
Alisson Neves <apereira@thoughtworks.com>
2018-08-28 11:52:40 -0300

    Ajustar o retorno nas condições em caso de erro na execução dos comandos

--- src/tests/functional/runSmokeTest.sh
+++ src/tests/functional/runSmokeTest.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 echo Instalando o Protractor globalmente...
-
 yarn global add protractor
+
+# if [ "$?" -ne "0" ]; then
+#     echo A instalação do Protractor falhou e retornou o código "$?"!
+#     exit "$?"
+# fi
 
 if [ "$?" -eq "0" ]; then
     echo Atualizando o servidor do Selenium...
     webdriver-manager update
 else
-    echo A atualização do servidor falhou e retornou o código "$?"!
+    echo A instalação do Protractor falhou e retornou o código "$?"!
+    # echo A atualização do servidor falhou e retornou o código "$?"!
     exit "$?"
 fi
 
@@ -16,7 +21,8 @@ if [ "$?" -eq "0" ]; then
     echo Iniciando o servidor...
     webdriver-manager start --detach
 else
-    echo A iniciação do servidor falhou e retornou o código "$?"!
+    # echo A iniciação do servidor falhou e retornou o código "$?"!
+    echo A atualização do servidor falhou e retornou o código "$?"!
     exit "$?"
 fi
 
@@ -24,7 +30,8 @@ if [ "$?" -eq "0" ]; then
     echo Executando os testes...
     protractor functional-tests.conf.js
 else
-    echo A execução dos testes falhou e retornou o código "$?"!
+    # echo A execução dos testes falhou e retornou o código "$?"!
+    echo A iniciação do servidor falhou e retornou o código "$?"!
     exit "$?"
 fi
 
@@ -32,8 +39,13 @@ if [ "$?" -eq "0" ]; then
     echo Parando o servidor...
     webdriver-manager shutdown
 else
-    echo O encerramento do servidor falhou e retornou o código "$?"!
+    echo A execução dos testes falhou e retornou o código "$?"!
+    # echo O encerramento do servidor falhou e retornou o código "$?"!
     exit "$?"
 fi
 
-exit
+if [ "$?" -eq "0" ]; then
+    exit
+else
+    exit "$?"
+fi

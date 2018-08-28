4e07503e3963926ae2582e66425a3890ceacbb01
Alisson Neves <apereira@thoughtworks.com>
2018-08-28 14:13:49 -0300

    Ajustar as condicionais para obter o retorno correto

--- src/tests/functional/runSmokeTest.sh
+++ src/tests/functional/runSmokeTest.sh
@@ -6,33 +6,29 @@ yarn global add protractor
 if [ "$?" -eq "0" ]; then
     echo Atualizando o servidor do Selenium...
     webdriver-manager update
-    else
-    echo A instalação do Protractor falhou e retornou o código "$?"!
-
     if [ "$?" -eq "0" ]; then
         echo Iniciando o servidor...
         webdriver-manager start --detach
-        else
-        echo A atualização do servidor falhou e retornou o código "$?"!
-
         if [ "$?" -eq "0" ]; then
             echo Executando os testes...
             protractor functional-tests.conf.js
-            else
-            echo A iniciação do servidor falhou e retornou o código "$?"!
-
             if [ "$?" -eq "0" ]; then
                 echo Parando o servidor...
                 webdriver-manager shutdown
-                else
-                echo A execução dos testes falhou e retornou o código "$?"!
-
                 if [ "$?" -eq "0" ]; then
                     echo O servidor foi parado com sucesso!
                     else
                     echo O encerramento do servidor falhou e retornou o código "$?"!
                 fi
+                else
+                echo A execução dos testes falhou e retornou o código "$?"!
             fi
+            else
+            echo A iniciação do servidor falhou e retornou o código "$?"!
         fi
+        else
+        echo A atualização do servidor falhou e retornou o código "$?"!
     fi
+    else
+    echo A instalação do Protractor falhou e retornou o código "$?"!
 fi

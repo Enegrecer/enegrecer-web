f4782d7b96d3675ea80d54e9a9b9ee2ccfc5e9f5
Alisson Neves <apereira@thoughtworks.com>
2018-08-28 13:22:24 -0300

    Ajustar o retorno dos comandos no shell script para testar o status de execução do mesmo

--- src/tests/functional/runSmokeTest.sh
+++ src/tests/functional/runSmokeTest.sh
@@ -3,36 +3,28 @@
 echo Instalando o Protractor globalmente...
 yarn global add protractor
 
-# if [ "$?" -ne "0" ]; then
-#     echo A instalação do Protractor falhou e retornou o código "$?"!
-#     exit "$?"
-# fi
-
 if [ "$?" -eq "0" ]; then
     echo Atualizando o servidor do Selenium...
     webdriver-manager update
 else
     echo A instalação do Protractor falhou e retornou o código "$?"!
-    # echo A atualização do servidor falhou e retornou o código "$?"!
-    exit "$?"
+    # exit "$?"
 fi
 
 if [ "$?" -eq "0" ]; then
     echo Iniciando o servidor...
     webdriver-manager start --detach
 else
-    # echo A iniciação do servidor falhou e retornou o código "$?"!
     echo A atualização do servidor falhou e retornou o código "$?"!
-    exit "$?"
+    # exit "$?"
 fi
 
 if [ "$?" -eq "0" ]; then
     echo Executando os testes...
     protractor functional-tests.conf.js
 else
-    # echo A execução dos testes falhou e retornou o código "$?"!
     echo A iniciação do servidor falhou e retornou o código "$?"!
-    exit "$?"
+    # exit "$?"
 fi
 
 if [ "$?" -eq "0" ]; then
@@ -40,12 +32,13 @@ if [ "$?" -eq "0" ]; then
     webdriver-manager shutdown
 else
     echo A execução dos testes falhou e retornou o código "$?"!
-    # echo O encerramento do servidor falhou e retornou o código "$?"!
-    exit "$?"
+    # exit "$?"
 fi
 
 if [ "$?" -eq "0" ]; then
-    exit
+    echo O servidor foi parado com sucesso!
+    # exit
 else
-    exit "$?"
+    echo O encerramento do servidor falhou e retornou o código "$?"!
+    # exit "$?"
 fi

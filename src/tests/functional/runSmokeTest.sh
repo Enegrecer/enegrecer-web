6a0f47feb06632b8ed2b045526e9ebe7262cfbb7
Alisson Neves <apereira@thoughtworks.com>
2018-08-29 10:49:31 -0300

    Identificar variável para retorno de execução de cada comando

--- src/tests/functional/runSmokeTest.sh
+++ src/tests/functional/runSmokeTest.sh
@@ -3,38 +3,42 @@
 echo Instalando o Protractor globalmente...
 yarn global add protractor
 
-export StatusResult=$?;
-if [ $StatusResult -eq "0" ]; then
+export StatusResult1=$?;
+if [ $StatusResult1 -eq "0" ]; then
     echo Atualizando o servidor do Selenium...
     webdriver-manager update
-    if [ $StatusResult -eq "0" ]; then
+    export StatusResult2=$?;
+    if [ $StatusResult2 -eq "0" ]; then
         echo Iniciando o servidor...
         webdriver-manager start --detach
-        if [ $StatusResult -eq "0" ]; then
+        export StatusResult3=$?;
+        if [ $StatusResult3 -eq "0" ]; then
             echo Executando os testes...
             protractor functional-tests.conf.js
-            if [ $StatusResult -eq "0" ]; then
+            export StatusResult4=$?;
+            if [ $StatusResult4 -eq "0" ]; then
                 echo Parando o servidor...
                 webdriver-manager shutdown
-                if [ $StatusResult -eq "0" ]; then
+                export StatusResult5=$?;
+                if [ $StatusResult5 -eq "0" ]; then
                     echo O servidor foi parado com sucesso!
                     else
-                    echo O encerramento do servidor falhou e retornou o código $StatusResult!
-                    exit $StatusResult
+                    echo O encerramento do servidor falhou e retornou o código $StatusResult5!
+                    exit $StatusResult5
                 fi
                 else
-                echo A execução dos testes falhou e retornou o código $StatusResult!
-                exit $StatusResult
+                echo A execução dos testes falhou e retornou o código $StatusResult4!
+                exit $StatusResult4
             fi
             else
-            echo A iniciação do servidor falhou e retornou o código $StatusResult!
-            exit $StatusResult
+            echo A iniciação do servidor falhou e retornou o código $StatusResult3!
+            exit $StatusResult3
         fi
         else
-        echo A atualização do servidor falhou e retornou o código $StatusResult!
-        exit $StatusResult
+        echo A atualização do servidor falhou e retornou o código $StatusResult2!
+        exit $StatusResult2
     fi
     else
-    echo A instalação do Protractor falhou e retornou o código $StatusResult!
-    exit $StatusResult
+    echo A instalação do Protractor falhou e retornou o código $StatusResult1!
+    exit $StatusResult1
 fi

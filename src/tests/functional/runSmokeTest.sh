7d72e3e8ecb0e9b6ff3d073f997363cc5d5e2ee9
Nayara <nsouza@thoughtworks.com>
2019-08-10 20:04:41 -0300

    SEM-ISSUE - Adiciona cores aos logs do script dos testes funcionais

--- src/tests/functional/runSmokeTest.sh
+++ src/tests/functional/runSmokeTest.sh
@@ -1,44 +1,49 @@
 #!/bin/sh
 
-echo Instalando o Protractor globalmente...
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e ${PURPLE}"\n\nInstalando o Protractor globalmente..."${NC}
 yarn global add protractor
 
 export StatusResult1=$?;
 if [ $StatusResult1 -eq "0" ]; then
-    echo Atualizando o servidor do Selenium...
+    echo -e ${PURPLE}"\n\nAtualizando o servidor do Selenium..."${NC}
     webdriver-manager update
     export StatusResult2=$?;
     if [ $StatusResult2 -eq "0" ]; then
-        echo Iniciando o servidor...
+        echo -e ${PURPLE}"\n\nIniciando o servidor..."${NC}
         webdriver-manager start --detach
         export StatusResult3=$?;
         if [ $StatusResult3 -eq "0" ]; then
-            echo Executando os testes...
+            echo -e ${PURPLE}"\n\nExecutando os testes..."${NC}
             protractor functional-tests.conf.js
             export StatusResult4=$?;
             if [ $StatusResult4 -eq "0" ]; then
-                echo Parando o servidor...
+                echo -e ${PURPLE}"\n\nParando o servidor..."${NC}
                 webdriver-manager shutdown
                 export StatusResult5=$?;
                 if [ $StatusResult5 -eq "0" ]; then
-                    echo O servidor foi parado com sucesso!
+                    echo -e ${GREEN}"\n\nO servidor foi parado com sucesso!"${NC}
                     else
-                    echo O encerramento do servidor falhou e retornou o código $StatusResult5!
+                    echo -e ${RED}"\n\nO encerramento do servidor falhou e retornou o código $StatusResult5!"${NC}
                     exit $StatusResult5
                 fi
                 else
-                echo A execução dos testes falhou e retornou o código $StatusResult4!
+                echo -e ${RED}"\n\nA execução dos testes falhou e retornou o código $StatusResult4!"${NC}
                 exit $StatusResult4
             fi
             else
-            echo A iniciação do servidor falhou e retornou o código $StatusResult3!
+            echo -e ${RED}"\n\nA iniciação do servidor falhou e retornou o código $StatusResult3!"${NC}
             exit $StatusResult3
         fi
         else
-        echo A atualização do servidor falhou e retornou o código $StatusResult2!
+        echo -e ${RED}"\n\nA atualização do servidor falhou e retornou o código $StatusResult2!"${NC}
         exit $StatusResult2
     fi
     else
-    echo A instalação do Protractor falhou e retornou o código $StatusResult1!
+    echo -e ${RED}"\n\nA instalação do Protractor falhou e retornou o código $StatusResult1!"${NC}
     exit $StatusResult1
 fi
